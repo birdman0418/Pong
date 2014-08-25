c5d1b6c5dfc8f959ee8c6732b1473dedcb1cf29c
Brandon <Home@Bensens-MacBook-Air.local>
2014-08-25 16:19:23 -0700

    Initial Commit

---
+++ Pong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by Brandon on 8/25/14.
+//  Copyright (c) 2014 Brandon Bensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pong/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pong
+//
+//  Created by Brandon on 8/25/14.
+//  Copyright (c) 2014 Brandon Bensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
